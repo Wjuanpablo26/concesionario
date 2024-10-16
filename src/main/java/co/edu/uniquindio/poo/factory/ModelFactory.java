f9125198694cfa014829e9e04dacf398c7376455
Wjuanpablo26 <juanp.reyg@uqvirtual.edu.co>
2024-10-15 20:04:28 -0500

    Creación clases de paquetes y reestructutración 'Persona'

---
+++ src/main/java/co/edu/uniquindio/poo/factory/ModelFactory.java
@@ -0,0 +1,5 @@
+package co.edu.uniquindio.poo.factory;
+
+public class ModelFactory {
+    
+}
---
+++ src/main/java/co/edu/uniquindio/poo/viewcontroller/ViewController.java
@@ -0,0 +1,5 @@
+package co.edu.uniquindio.poo.viewcontroller;
+
+public class ViewController {
+    
+}
